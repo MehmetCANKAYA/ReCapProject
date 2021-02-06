544fb2cfa06ad887b55e9d2751d46acdb5669f45
Mehmet <Mehmet@DESKTOP-MJ5N9AL>
2021-02-06 15:52:56 +0300

    ReCapProject_1Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,33 @@
+﻿using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            foreach (var car in carManager.GetAll())            
+                Console.WriteLine($"{car.BrandId} {car.ColorId} {car.Description} {car.DailyPrice} ");                 
+            
+
+            Console.WriteLine("-----------------------------");
+            foreach (var car in carManager.GetAllByColor(2))            
+                Console.WriteLine($"{car.BrandId} {car.ColorId} {car.Description} {car.DailyPrice} ");
+            
+
+            carManager.Add(new Entities.Concrete.Car { CarId = 9, BrandId = 2, ColorId = 1, Description = "yeni Kayıt", DailyPrice = 134000, year = "2020" });
+
+            Console.WriteLine((carManager.Get(9)).Description);
+            Console.WriteLine("-----------------------------");
+            foreach (var car in carManager.GetAll())            
+                Console.WriteLine($"{car.BrandId} {car.ColorId} {car.Description} {car.DailyPrice} ");
+            
+
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,65 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        //Private
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{CarId=1, BrandId=1, ColorId=1, DailyPrice=10000, Description="Makam Aracı - Mercedes", year="2018"},
+                new Car{CarId=2, BrandId=1, ColorId=2, DailyPrice=2000, Description="Ticari Arac - Mercedes", year="2010"},
+                new Car{CarId=3, BrandId=2, ColorId=2, DailyPrice=13000, Description="Makam Aracı - BMW", year="2020"},
+                new Car{CarId=4, BrandId=1, ColorId=1, DailyPrice=7000, Description="Panelvan - Mercedes", year="2021"},
+                new Car{CarId=5, BrandId=1, ColorId=2, DailyPrice=10000, Description="B sınıfı araç- Mercedes", year="2019"},
+                new Car{CarId=6, BrandId=2, ColorId=2, DailyPrice=4000, Description="E sınıfı- BMW", year="2018"},
+            };
+
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Update(Car car)
+        {
+            var updatedCar = _cars.SingleOrDefault(p=>p.CarId == car.CarId);
+            updatedCar.BrandId = car.BrandId;
+            updatedCar.ColorId = car.ColorId;
+            updatedCar.DailyPrice = car.DailyPrice;
+            updatedCar.Description = car.Description;
+            updatedCar.year = car.year;
+        }
+        public void Delete(Car car)
+        {
+            var deletedCar = _cars.SingleOrDefault(p=>p.CarId == car.CarId);
+            _cars.Remove(deletedCar);
+        }
+
+        public Car Get(int carId)
+        {
+            return _cars.SingleOrDefault(p=>p.CarId == carId);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetAllByColor(int colorId)
+        {
+            return _cars.Where(p => p.ColorId == colorId).ToList();
+
+        }
+    }
+}
